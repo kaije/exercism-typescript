699d886e1a318441aade690b8861e5825986e0ea
Kelly Farlow <kelly.farlow@gmail.com>
2019-06-24 13:58:02 +1000

    Refactor

--- word-count/word-count.ts
+++ word-count/word-count.ts
@@ -1,9 +1,13 @@
 export default class Words {
   count(input: string): Map<string, number> {
     const counts = new Map();
-    const items = input.trim().toLowerCase();
-    const itemArr = items.split(/\s+/);
-    itemArr.forEach((word) => counts.set(word, counts.get(word) + 1 || 1));
+
+    const words = input
+      .trim()
+      .toLowerCase()
+      .split(/\s+/);
+
+    words.forEach(word => counts.set(word, counts.get(word) + 1 || 1));
 
     return counts;
   }
